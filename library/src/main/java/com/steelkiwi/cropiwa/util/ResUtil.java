99a59b6e3038a0439d31531d745d77fb661a9b68
Alon Parker <alonparker87@gmail.com>
2019-06-25 16:48:24 +0500

    fix oom with big bitmap size

--- library/src/main/java/com/steelkiwi/cropiwa/util/ResUtil.java
+++ library/src/main/java/com/steelkiwi/cropiwa/util/ResUtil.java
@@ -5,6 +5,8 @@ import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DimenRes;
 import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 /**
  * @author yarolegovich https://github.com/yarolegovich
@@ -25,5 +27,20 @@ public class ResUtil {
 
     public int dimen(@DimenRes int dimRes) {
         return Math.round(context.getResources().getDimension(dimRes));
+    }
+
+    public static int getWidthScreen(Context context) {
+        return getDisplayMetrics(context).widthPixels;
+    }
+
+    public static int getHeightScreen(Context context) {
+        return getDisplayMetrics(context).heightPixels;
+    }
+
+    private static DisplayMetrics getDisplayMetrics(Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics;
     }
 }
